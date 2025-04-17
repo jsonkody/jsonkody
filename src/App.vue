6817e2320fda7185b2ae78b2d880706fb117f13d
JsonKody <daniel8cech@gmail.com>
2025-04-17 17:23:52 +0200

    saved by pnpm

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // const jk = `<img src="/images/jk.svg" class="rounded-full">`;
 const bekinka = `<img src="/images/bekinka.avif" class="rounded-full">`;
 const num = `<img src="/images/num.avif" class="rounded-full">`;
-const npm = `<img src="/images/npm.avif" class="rounded-full">`;
+const toolpop = `<img src="/images/toolpop.avif" class="rounded-full">`;
 const snejk = `<img src="/images/snejk.avif" class="rounded-full">`;
 const github = `<img src="/images/github.avif" class="rounded-full">`;
 const discord = `<img src="/images/discord.avif" class="rounded-full">`;
@@ -49,7 +49,7 @@ const youtube = `<img src="/images/jk.svg" width="120" height="120" class="round
           class="main-link text-white hover:text-blue-300"
           href="https://www.npmjs.com/package/toolpop"
           target="_blank"
-          v-pop.html="npm"
+          v-pop.html="toolpop"
         >
           toolpop
         </a>
