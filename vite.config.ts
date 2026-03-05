2e98dde6c22a1b23a3a80624d3d7bb889e5c9832
JsonKody <daniel8cech@gmail.com>
2026-03-05 12:06:46 +0100

    autosave

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import vueDevTools from "vite-plugin-vue-devtools";
-import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    viteCompression({
-      algorithm: "brotliCompress",
-    }),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -24,42 +16,7 @@ export default defineConfig({
     port: 1212,
   },
   build: {
-    minify: "terser",
     sourcemap: false,
     cssCodeSplit: true,
-    reportCompressedSize: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Advanced chunk splitting strategy
-          if (id.includes("node_modules")) {
-            // priradí knihovny do samostatných chunku podle názvu balícku
-            const modules = [
-              "vue",
-              "vue-router",
-              "pinia",
-              "lodash",
-              "axios",
-              "@floating-ui/dom",
-              // a další knihovny, které by mohly tvorit významné cásti vašeho kódu
-            ];
-
-            const chunkName = modules.find((module) => id.includes(module));
-            return chunkName ? `vendor-${chunkName}` : "vendor-others";
-          }
-        },
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js",
-      },
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        pure_funcs: ["console.debug"], // Keep warnings/errors
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["vue", "vue-router", "pinia", "@heroicons/vue"],
   },
 });
